c079fac343b9d153e31734b6fcc2b195e08ae625
Osaid10 <khanosaid22@gmail.com>
2024-05-10 04:10:10 +0500

    Added Code

--- dogvscat_image_classifier.py
+++ dogvscat_image_classifier.py
@@ -10,3 +10,17 @@ Image_Width=128
 Image_Height=128
 Image_Size=(Image_Width,Image_Height)
 Image_Channels=3
+filenames=os.listdir("E:\Study\GIKI BAI Course Material\Fourth Semester BAI\AI202\Osaid\\train\\train")
+
+categories=[]
+for f_name in filenames:
+    category=f_name.split('.')[0]
+    if category=='dog':
+        categories.append(1)
+    else:
+        categories.append(0)
+
+df=pd.DataFrame({
+    'filename':filenames,
+    'category':categories
+})
