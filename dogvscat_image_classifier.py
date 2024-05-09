3a71d3cbd526f5aa7e770fecd292161e64bb8056
Osaid10 <khanosaid22@gmail.com>
2024-05-10 04:37:38 +0500

    Added Code

--- dogvscat_image_classifier.py
+++ dogvscat_image_classifier.py
@@ -148,3 +148,4 @@ test_df = pd.DataFrame({
     'filename': test_filenames
 })
 nb_samples = test_df.shape[0]
+predict = model.predict_generator(test_generator, steps=np.ceil(nb_samples/batch_size))\
